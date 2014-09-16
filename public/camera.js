912d3eb44f4e6ceb90e3c3b7f9510f396845c3d1
Uday Verma <uday.karan@gmail.com>
2014-09-16 14:12:01 -0500

    More stuff

--- public/camera.js
+++ public/camera.js
@@ -2,52 +2,13 @@
 
 var React = require('react');
 
-function getMediaStream(callback) {
-  navigator.getUserMedia = (navigator.getUserMedia ||
-    navigator.webkitGetUserMedia ||
-    navigator.mozGetUserMedia ||
-    navigator.msGetUserMedia);
-  if (navigator.getUserMedia) {
-    navigator.getUserMedia({
-        video: true,
-        audio: true
-      },
-      function(localMediaStream) {
-        callback(null, window.URL.createObjectURL(localMediaStream));
-
-      },
-      function(err) {
-        console.log("The following error occured: " + err);
-      }
-    );
-  } else {
-    console.log("getUserMedia not supported");
-  }
-}
-
-
 module.exports = React.createClass({
+    componentDidMount: function() {
+        console.log(this.refs);
+        this.refs.video.getDOMNode().play();
+    },
 
-  getInitialState: function() {
-    return {
-      "mediaStream": null
-    };
-  },
-
-  componentDidMount: function() {
-    var self = this;
-    getMediaStream(function(err, stream) {
-      self.setState({
-          "mediaStream": stream
-        },
-        function() {
-          self.refs.video.getDOMNode().play();
-        });
-
-    });
-  },
-
-  render: function() {
-    return <video ref="video" src={this.state.mediaStream}/>;
-  }
+    render: function() {
+        return <video ref="video" style={{top: (this.props.index * 200) + "px" }} src={this.props.stream}/>;
+    }
 });
--- public/main.js
+++ public/main.js
@@ -6,32 +6,106 @@ var CodeMirror = require('code-mirror/mode/htmlmixed');
 
 var Camera = require('./Camera');
 
+var makePeer = function() {
+    var peer = window.peer = new Peer({
+        host: 'p2p.saskavi.com',
+        port: 9000,
+        key: 'saskavi',
+        debug: 3,
+        config: customConfig
+    });
+
+    return peer;
+};
+
+function getMediaStream(callback) {
+  navigator.getUserMedia = (navigator.getUserMedia ||
+    navigator.webkitGetUserMedia ||
+    navigator.mozGetUserMedia ||
+    navigator.msGetUserMedia);
+  if (navigator.getUserMedia) {
+    navigator.getUserMedia({
+        video: true,
+        audio: true
+      },
+      function(localMediaStream) {
+        callback(null, window.URL.createObjectURL(localMediaStream));
+
+      },
+      function(err) {
+        console.log("The following error occured: " + err);
+      }
+    );
+  } else {
+    console.log("getUserMedia not supported");
+  }
+}
+
+
+
 var Editor = React.createClass({
   getInitialState: function() {
     return {
-      'value': 'print(hello world)'
+      'value': 'print(hello world)',
     }
   },
 
   componentDidMount: function() {
-    this.cm = CodeMirror(this.refs.editor.getDOMNode(), {
-      value: this.state.value
-    });
-    this.cm.on("change", function() {
-      console.log(arguments)
-    });
+      var self = this;
+
+      this.cm = CodeMirror(this.refs.editor.getDOMNode(), {
+          value: this.state.value
+      });
+      this.cm.on("change", function(cm) {
+          self.setState({'value': cm.getValue()});
+      });
   },
 
   render: function() {
-    return <div ref="editor" class="editor"></div>;
+      return <div ref="editor" class="editor"></div> ;
   }
 });
 
 
-
 var Workspace = React.createClass({
+    getInitialState: function() {
+        return {
+            'streams': [],
+            'peer': null,
+            'conns': []
+        };
+    },
+
+    componentDidMount: function() {
+        var self = this;
+        getMediaStream(function(err, stream) {
+            var peer = makePeer();
+            window.peer = peer;
+            self.setState({streams: self.state.streams.concat([stream]), 'peer': peer});
+
+            peer.on('call', function(call) {
+                call.answer(localstream);
+                call.on('stream', function(stream) {
+                    console.log("got stream");
+                    self.setState({streams: self.state.streams.concat([stream])});
+                });
+            });
+        });
+    },
+
   render: function() {
-    return <div><Editor/><Camera/></div>
+      if (this.state.streams.length === 0) {
+          return <h1>Initializing ...</h1> ;
+      }
+
+      var cameras = this.state.streams.map(function(s, i) {
+          return <Camera stream={s} offset={i} /> ;
+      });
+
+      return <div>
+          <Editor/>
+          { cameras }
+      </div> ;
   }
 });
 
